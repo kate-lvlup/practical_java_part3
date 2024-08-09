9c8d0ab92988ed0b21876a9d7bc4e42f4f7086f8
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-09 11:22:06 +0300

    add task2 iterate names in array using iterator

--- src/main/java/jom/com/softserve/s3/task2/NameList.java
+++ src/main/java/jom/com/softserve/s3/task2/NameList.java
@@ -1,11 +1,30 @@
 package jom.com.softserve.s3.task2;
 
+import java.util.NoSuchElementException;
+
 class NameList {
-	private final String[] names = { "Mike", "Emily", "Nick", "Patric", "Sara" };
+    private final String[] names = {"Mike", "Emily", "Nick", "Patric", "Sara"};
 
-//	public Iterator getIterator() {
-//		return new Iterator();
-//	}
+    public Iterator getIterator() {
+        return new Iterator();
+    }
 
-	// Write your code here
+    public class Iterator {
+        private int counter = 0;
+
+        private Iterator() {
+        }
+
+        public boolean hasNext() {
+            return counter < names.length;
+        }
+
+        public String next() {
+            if (hasNext()) {
+                return names[counter++];
+            } else {
+                throw new NoSuchElementException("No more names in the list");
+            }
+        }
+    }
 }
