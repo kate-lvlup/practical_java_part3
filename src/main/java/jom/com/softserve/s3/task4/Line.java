742e22a1e8a31121f32d4fcbf6274e19ee2d3d6c
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-09 16:09:29 +0300

    add task4 drawLine SOLID, DOTTED, DASHED and DOUBLE

--- src/main/java/jom/com/softserve/s3/task4/Line.java
+++ src/main/java/jom/com/softserve/s3/task4/Line.java
@@ -1,7 +1,20 @@
 package jom.com.softserve.s3.task4;
 
-//Describe LineType enum here
-
 public class Line {
-	// Write method code here
+    public static String drawLine(LineType lineType) {
+        switch (lineType) {
+            case SOLID:
+                return "The line is solid type.";
+            case DOTTED:
+                return "The line is dotted type.";
+            case DASHED:
+                return "The line is dashed type.";
+            case DOUBLE:
+                return "The line is double type.";
+            default:
+                throw new IllegalArgumentException("Unknown line type: " + lineType);
+        }
+    }
+
 }
+
---
+++ src/main/java/jom/com/softserve/s3/task4/LineType.java
@@ -0,0 +1,5 @@
+package jom.com.softserve.s3.task4;
+
+public enum LineType {
+    SOLID, DOTTED, DASHED, DOUBLE
+}
