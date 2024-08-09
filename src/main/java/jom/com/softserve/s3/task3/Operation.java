054881d1cb6f005e5016d5eba0818978e63dfdcc
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-09 15:55:49 +0300

    add task3 execute method 4 times Using anonymous classes

--- src/main/java/jom/com/softserve/s3/task3/Operation.java
+++ src/main/java/jom/com/softserve/s3/task3/Operation.java
@@ -2,22 +2,39 @@ package jom.com.softserve.s3.task3;
 
 public class Operation {
 	public static double addAtoB(int a, int b) {
-		// Write your code here
-		return 0;
+//		return execute(a, b, (x, y) -> Operation.addAtoB(x, y));
+		return a + b;
 	}
 
 	public static double subtractBfromA(int a, int b) {
-		// Write your code here
-		return 0;
+//		return execute(a, b, (x, y) -> Operation.subtractBfromA(x, y));
+		return a - b;
 	}
 
 	public static double multiplyAbyB(int a, int b) {
-		// Write your code here
-		return 0;
+//		return execute(a, b, (x, y) -> Operation.multiplyAbyB(x, y));
+		return a * b;
 	}
 
 	public static double divideAbyB(int a, int b) {
-		// Write your code here
-		return 0;
+//		return execute(a, b, (x, y) -> {
+//			if (y != 0) {
+//				return Operation.divideAbyB(x, y);
+//			} else {
+//				throw new ArithmeticException("Division by zero is not allowed.");
+//			}
+//		});
+		if (b != 0) {
+			return (double) (a / b); // Perform integer division and cast to double
+		} else {
+			throw new ArithmeticException("Division by zero is not allowed.");
+		}
+	}
+
+
+	public static double execute(int a,int b,Strategy strategy){
+		double result = strategy.doOperation(a,b);
+		System.out.println(result);
+		return result;
 	}
 }
