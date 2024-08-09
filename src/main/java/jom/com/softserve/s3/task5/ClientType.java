492d03717d0ffa8af717812e7360831ac808f5dd
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-09 16:21:17 +0300

    add task5 enum ClientType- NEW SILVER GOLD PLATINUM ->  method discount()

--- src/main/java/jom/com/softserve/s3/task5/ClientType.java
+++ src/main/java/jom/com/softserve/s3/task5/ClientType.java
@@ -1,5 +1,44 @@
 package jom.com.softserve.s3.task5;
 
 enum ClientType {
-	// Write your code here
+    NEW(1) {
+        @Override
+        public double discount() {
+            return 1.0;
+        }
+    },
+    SILVER(12) {
+        @Override
+        public double discount() {
+            return (100 - 12 * 0.25) / 100.0;
+        }
+    },
+    GOLD(30) {
+        @Override
+        public double discount() {
+            return (100 - 30 * 0.3) / 100.0;
+        }
+    },
+    PLATINUM(60) {
+        @Override
+        public double discount() {
+            return (100 - 60 * 0.35) / 100.0;
+        }
+    };
+
+    private final int months;
+
+    ClientType(int months) {
+        this.months = months;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+    public double discount() {
+        return 1.0;
+    }
 }
+
+
